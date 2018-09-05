46e2af51ec83797bfe4e4dc86d4d73d01b1169c1
Tom Harmon <thomas.harmon@gmail.com>
2018-09-04 23:48:55 -0500

    Discussed potential solution with classmate - incorporating

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -1,85 +1,48 @@
-#functions
-    #pull list of unique names - create index 
-
-
-#read csv file
-#run  function that pulls a list of unique names 
-#create dictionary out of list that includes
-    #Candidate name
-    #votes
-    #
-
-
 import os
 import csv
 
-#Create empty dictionary for candidates - build dictionary from dataset
+csvread= os.path.join("election_data.csv")
 
+#list for candidate names
+voting_data = []
 
-# candidates_dict = {"name" = #something from }
+#variable for total votes
+total_votes = 0
 
-candidate_list = []
-votes = []
-
-
-total_votecount = 0
-khan_votecount = 0
-li_votecount = 0
-otooley_votecount = 0
-correy_votecount = 0
-winner = ""
-
-#Pull from CSV 
-
-electioncsv = os.path.join("election_data.csv")
-
-with open(electioncsv, newline="") as csvfile:
-
+#open/read file 
+with open(csvread, newline="") as csvfile:
     csvreader = csv.reader(csvfile, delimiter = ",")
-
-# #skip header row 
-
+    
+    #skip header
     header = next(csvreader)
 
-#function to find the names of the different candidates from column 1 and put in a dictionary? 
-
-#For Loop thru file and
-
+    #loop to put voting data in a python list
     for row in csvreader:
+        voting_data.append(row[2])
 
-        candidate_name = row[2]
-                
-        #count the total votes
-        # total_votecount = total_votecount + 1 
+#total votes is the total number of rows in list (len)
+total_votes=len(voting_data)
 
-        if candidate_name not in candidate_list:
-            candidate_list.append(row[2])
+#empty list to populate with final results - candidate name (candidate), % (candidate_percent), and candidiate vots (candidate_votes) 
+all_results = []
 
+#empty dict to sort final tallies to determine winner
+most_votes = {}
 
+#build a set of the candidates,
+candidates_set = sorted({*voting_data}, key=voting_data.count, reverse=True)
 
-        #count khan's votes
+#get candidate data
+for candidate in candidates_set: 
+    candidate_votes = voting_data.count(candidate)
+    candidate_percent = round((candidate_votes/total_votes)*100)
+    #put results in the list
+    all_results.append(f"{candidate}: {candidate_percent}% ({candidate_votes})")
+    #build dictionary for winner 
+    most_votes.update({candidate:candidate_votes})
 
-        # if row[2] == (candidate_list[0]):
-        #     khan_votecount = khan_votecount + 1
+winner = max(most_votes, key=most_votes.get)
 
-        # if row[2] == "Li":
-        #     li_votecount = li_votecount + 1
-
-        # if row[2] == "O'Tooley":
-        #     otooley_votecount = otooley_votecount + 1
-
-        # if row[2] == "Correy":
-        #     correy_votecount = correy_votecount + 1
-
-
-    #Add each unique candidate to list - append to list both a name and a vote count for that name
-            # if row[0] not in candidate_list:
-            #     candidate_list.append(row[0])
-
-
-    #Tally the number of votes each got - run a counter 
-    #Tally the total number of votes - run a counter 
-    #Calculate total candidate votes over total votes - % - calculate on the fly? 
 
     #Compare final numbers and make candidate with highest votes the Winner 
         #How do I compare values in a list as I go? 
@@ -100,8 +63,9 @@ with open(electioncsv, newline="") as csvfile:
 
 
 # print("Election Results")
-print(candidate_list) 
-print(candidate_list[1])
+# print(candidate_list) 
+print(str(total_votes))
+print(candidates_set)
 
 print("______________________")
 
@@ -125,3 +89,15 @@ print("______________________")
 #   -------------------------
 #   Winner: Khan
 #   -------------------------
+
+
+# khan_votecount = 0
+# li_votecount = 0
+# otooley_votecount = 0
+# correy_votecount = 0
+# winner = ""
+
+#function to find the names of the different candidates from column 1 and put in a dictionary? 
+
+#For Loop thru file 
+# #Add each unique candidate to list - append to list both a name and a vote count for that name
